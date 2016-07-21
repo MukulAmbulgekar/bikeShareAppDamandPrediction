64ef7a1dd84637c4c42d3fa1a118c7ae3c41f0ba
Mukul Ambulgekar <mukulambulgekar@gmail.com>
2016-07-20 19:19:06 -0700

    Added screenshot for demand prediction using Random Forest (SCI learn python)

--- bikeShare/www/demand.js
+++ bikeShare/www/demand.js
@@ -2,7 +2,7 @@ var newData = [];
 var dateSelected = 'Mar 23, 2015';
 var fromDate;
 var toDate;
-var stationIdSelected = 2;
+var stationIdSelected = 70;
 $(function() {
     getPrediction(dateSelected);
 })
@@ -13,12 +13,12 @@ function getPrediction(dateSelect) {
     fromDate = new Date(dateSelect);
     var toDate = new Date(dateSelect)
     toDate.setDate(toDate.getDate() + 1);
-    $.getJSON("/bikeShare/www/RandomForestTrips/" + stationIdSelected + "_Prediction_Trip_RF.json", function(data) {
+    $.getJSON("/bikeShare/www/RandomForestTripsScikit/" + stationIdSelected + "_Prediction_RF_SCI.json", function(data) {
         _.forEach(data, function(eachData) {
             eachData.time = new Date(eachData.time)
             if (eachData.time.getTime() > fromDate.getTime() && eachData.time.getTime() < toDate.getTime()) {
                 eachData.Predicted_Count = Math.round(eachData.Predicted_Count);
-                eachData.hours = eachData.time.getHours()
+                eachData.hours = eachData.time.getHours() + ':00'
                 newData.push(eachData);
             }
         })
@@ -37,22 +37,28 @@ function getPrediction(dateSelect) {
 function plotDemand() {
     $('#container').highcharts({
         title: {
-            text: 'Daily Demand Prediction for Trips on hourly basis - <b>' + newData[0].name + '</b><br> Correlation :<b>' + newData[0].COR + '</b> <br> RMSLE:<b>' + Math.round(newData[0].RMSLE * 100) / 100 + '</b>',
+            text: 'Daily Demand Prediction for Trips on hourly basis - <b>' + newData[0].name + '</b><br> Correlation :<b>' + newData[0].COR + '</b>  RMSLE:<b>' + Math.round(newData[0].RMSLE * 100) / 100 + '</b>',
             x: -20 //center
         },
         subtitle: {
-            text: 'Demand Prediction- <b>' + fromDate.getFullYear() + "/" + (fromDate.getMonth() + 1) + "/" + fromDate.getDate() + '</b> <br> Algorithm - <b>Gradient Boosting</b>'
+            text: 'Demand Prediction- <b>' + fromDate.getFullYear() + "/" + (fromDate.getMonth() + 1) + "/" + fromDate.getDate() + '</b> <br> Algorithm - <b>Random Forest</b>'
 
         },
         xAxis: {
             categories: _.pluck(newData, 'hours'),
             title: {
-                text: ' Hours [0-23]'
+                text: ' Hours [0-23]',
+                style: {
+                    fontSize: '16px'
+                }
             }
         },
         yAxis: {
             title: {
-                text: ' Total number of trips'
+                text: ' Total number of trips',
+                style: {
+                    fontSize: '16px'
+                }
             },
             plotLines: [{
                 value: 0,
@@ -61,7 +67,10 @@ function plotDemand() {
             }]
         },
         tooltip: {
-            valueSuffix: ' Trips'
+            valueSuffix: ' Trips',
+            style: {
+                fontSize: '16px'
+            }
         },
         legend: {
             layout: 'vertical',
@@ -78,3 +87,113 @@ function plotDemand() {
         }]
     });
 }
+
+/**
+ * Sand-Signika theme for Highcharts JS
+ * @author Torstein Honsi
+ */
+
+// Load the fonts
+Highcharts.createElement('link', {
+    href: 'https://fonts.googleapis.com/css?family=Signika:400,700',
+    rel: 'stylesheet',
+    type: 'text/css'
+}, null, document.getElementsByTagName('head')[0]);
+
+// Add the background image to the container
+Highcharts.wrap(Highcharts.Chart.prototype, 'getContainer', function(proceed) {
+    proceed.call(this);
+    this.container.style.background = 'url(http://www.highcharts.com/samples/graphics/sand.png)';
+});
+
+
+Highcharts.theme = {
+    colors: ["#f45b5b", "#8085e9", "#8d4654", "#7798BF", "#aaeeee", "#ff0066", "#eeaaee",
+        "#55BF3B", "#DF5353", "#7798BF", "#aaeeee"
+    ],
+    chart: {
+        backgroundColor: "#aaeeee",
+        style: {
+            fontFamily: "Signika, serif"
+        }
+    },
+    title: {
+        style: {
+            color: 'black',
+            fontSize: '20px',
+            // fontWeight: 'bold'
+        }
+    },
+    subtitle: {
+        style: {
+            color: 'black',
+            fontSize: '16px',
+        }
+    },
+    tooltip: {
+        borderWidth: 0,
+        fontSize: '14px'
+    },
+    legend: {
+        itemStyle: {
+            fontWeight: 'bold',
+            fontSize: '18px'
+        }
+    },
+    xAxis: {
+        labels: {
+            style: {
+                color: '#6e6e70',
+                fontSize: '20px'
+            }
+        }
+    },
+    yAxis: {
+        labels: {
+            style: {
+                color: '#6e6e70',
+                fontSize: '20px'
+            }
+        }
+    },
+    plotOptions: {
+        series: {
+            shadow: true
+        },
+        candlestick: {
+            lineColor: '#404048'
+        },
+        map: {
+            shadow: false
+        }
+    },
+
+    // Highstock specific
+    navigator: {
+        xAxis: {
+            gridLineColor: '#D0D0D8'
+        }
+    },
+    rangeSelector: {
+        buttonTheme: {
+            fill: 'white',
+            stroke: '#C0C0C8',
+            'stroke-width': 1,
+            states: {
+                select: {
+                    fill: '#D0D0D8'
+                }
+            }
+        }
+    },
+    scrollbar: {
+        trackBorderColor: '#C0C0C8'
+    },
+
+    // General
+    background2: '#E0E0E8'
+
+};
+
+// Apply the theme
+Highcharts.setOptions(Highcharts.theme);
